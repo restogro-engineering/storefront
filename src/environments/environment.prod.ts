ab7852bc2af292363328e058c4fee573e966cf30
maniyedidi <yedidi-satya.manikanta@sixt.com>
2021-11-18 17:50:07 +0530

    updated port

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
-    apiHost: 'http://13.235.17.138:3000',
-    apiPort: 443,
+    apiHost: 'http://13.235.17.138',
+    apiPort: 3000,
     shopApiPath: 'shop-api',
     baseHref: '/storefront/',
     tokenMethod: 'cookie',
