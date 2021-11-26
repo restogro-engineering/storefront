88b5dcd18804a7f8d428c9f6419aad64d9558be2
maniyedidi <yedidi-satya.manikanta@sixt.com>
2021-11-26 21:43:23 +0530

    added headers

--- src/app/core/providers/data/data.service.ts
+++ src/app/core/providers/data/data.service.ts
@@ -1,37 +1,51 @@
-import { Injectable } from '@angular/core';
-import { NetworkStatus, WatchQueryFetchPolicy } from '@apollo/client/core';
-import { Apollo } from 'apollo-angular';
-import { DocumentNode } from 'graphql';
-import { Observable } from 'rxjs';
-import { filter, map } from 'rxjs/operators';
+import { Injectable } from "@angular/core";
+import { NetworkStatus, WatchQueryFetchPolicy } from "@apollo/client/core";
+import { Apollo } from "apollo-angular";
+import { DocumentNode } from "graphql";
+import { Observable } from "rxjs";
+import { filter, map } from "rxjs/operators";
 
 @Injectable({
-    providedIn: 'root',
+    providedIn: "root"
 })
 export class DataService {
-
-    private readonly context =  {
-        headers: {},
+    private readonly context = {
+        headers: {
+            secure: false,
+            secureProxy: true
+        }
     };
 
-    constructor(private apollo: Apollo) { }
+    constructor(private apollo: Apollo) {}
 
-    query<T = any, V = any>(query: DocumentNode, variables?: V, fetchPolicy?: WatchQueryFetchPolicy): Observable<T> {
-        return this.apollo.watchQuery<T, V>({
-            query,
-            variables,
-            context: this.context,
-            fetchPolicy: fetchPolicy || 'cache-first',
-        }).valueChanges.pipe(
-            filter(result => result.networkStatus === NetworkStatus.ready),
-            map(response => response.data));
+    query<T = any, V = any>(
+        query: DocumentNode,
+        variables?: V,
+        fetchPolicy?: WatchQueryFetchPolicy
+    ): Observable<T> {
+        return this.apollo
+            .watchQuery<T, V>({
+                query,
+                variables,
+                context: this.context,
+                fetchPolicy: fetchPolicy || "cache-first"
+            })
+            .valueChanges.pipe(
+                filter(result => result.networkStatus === NetworkStatus.ready),
+                map(response => response.data)
+            );
     }
 
-    mutate<T = any, V = any>(mutation: DocumentNode, variables?: V): Observable<T> {
-        return this.apollo.mutate<T, V>({
-            mutation,
-            variables,
-            context: this.context,
-        }).pipe(map(response => response.data as T));
+    mutate<T = any, V = any>(
+        mutation: DocumentNode,
+        variables?: V
+    ): Observable<T> {
+        return this.apollo
+            .mutate<T, V>({
+                mutation,
+                variables,
+                context: this.context
+            })
+            .pipe(map(response => response.data as T));
     }
 }
