503a64d68a3a95dc0d8e721b4c3877d4837433c1
maniyedidi <yedidi-satya.manikanta@sixt.com>
2021-12-06 22:54:53 +0530

    prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiHost: 'https://13.235.17.138',
+    apiHost: 'http://13.235.17.138',
     apiPort: 3000,
     shopApiPath: 'shop-api',
     baseHref: '/',
