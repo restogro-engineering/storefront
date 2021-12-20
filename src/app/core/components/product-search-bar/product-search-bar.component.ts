47e1003394f95286fb8ab0a0118165ae49c72b4c
maniyedidi <yedidi-satya.manikanta@sixt.com>
2021-12-20 19:10:48 +0530

    auto search

--- src/app/core/components/product-search-bar/product-search-bar.component.ts
+++ src/app/core/components/product-search-bar/product-search-bar.component.ts
@@ -1,38 +1,77 @@
-import { ChangeDetectionStrategy, Component, Input, OnDestroy, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
-import { Subscription } from 'rxjs';
-import { debounceTime } from 'rxjs/operators';
+import {
+    ChangeDetectionStrategy,
+    Component,
+    Input,
+    OnDestroy,
+    OnInit
+} from "@angular/core";
+import { FormControl } from "@angular/forms";
+import { ActivatedRoute, Router } from "@angular/router";
+import { Subscription } from "rxjs";
+import { debounceTime } from "rxjs/operators";
+import { SEARCH_PRODUCTS } from "../product-list/product-list.graphql";
+import { DataService } from "../../providers/data/data.service";
 
 @Component({
-    selector: 'vsf-product-search-bar',
-    templateUrl: './product-search-bar.component.html',
-    styleUrls: ['./product-search-bar.component.scss'],
-    changeDetection: ChangeDetectionStrategy.OnPush,
+    selector: "vsf-product-search-bar",
+    templateUrl: "./product-search-bar.component.html",
+    styleUrls: ["./product-search-bar.component.scss"],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ProductSearchBarComponent implements OnInit, OnDestroy {
     /** If true, searches as you type */
     @Input() autoSearch = false;
-
-    searchTerm = new FormControl('');
+    results: any[] = [];
+    queryField: FormControl = new FormControl();
+    searchTerm = new FormControl("");
     private subscription: Subscription;
-    constructor(private route: ActivatedRoute, private router: Router) { }
+    constructor(
+        private route: ActivatedRoute,
+        private router: Router,
+        private dataService: DataService
+    ) {}
 
     ngOnInit() {
         if (this.autoSearch) {
-            this.subscription = this.searchTerm.valueChanges.pipe(
-                debounceTime(250),
-            ).subscribe(term => this.doSearch(term));
+            this.subscription = this.searchTerm.valueChanges
+                .pipe(debounceTime(250))
+                .subscribe(term => this.doSearch(term));
         }
+
+        this.subscription = this.searchTerm.valueChanges
+            .pipe(debounceTime(250))
+            .subscribe(term => this.search(term));
     }
 
+    search(term: any) {
+        if (term) {
+            this.dataService
+                .query<any>(SEARCH_PRODUCTS, {
+                    input: {
+                        term,
+                        groupByProduct: true,
+                        take: 6
+                    }
+                })
+                .subscribe(res => {
+                    if (res.search && res.search.items) {
+                        this.results = res.search.items.map(
+                            (i: any) => i.productName
+                        );
+                    }
+                });
+        } else {
+            this.results = [];
+        }
+    }
     doSearch(term: string) {
-        this.router.navigate(['/search'], {
+        this.results = [];
+        this.router.navigate(["/search"], {
             queryParams: { search: term },
             relativeTo: this.route,
-            queryParamsHandling: 'merge',
+            queryParamsHandling: "merge"
         });
-        this.searchTerm.setValue('', { emitEvent: false });
+        this.searchTerm.setValue("", { emitEvent: false });
     }
 
     ngOnDestroy(): void {
@@ -40,5 +79,4 @@ export class ProductSearchBarComponent implements OnInit, OnDestroy {
             this.subscription.unsubscribe();
         }
     }
-
 }
